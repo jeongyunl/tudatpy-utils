7af4d8f5baa98823a2c9df9cd50b235c4be61603
Jeongyun Lee <jlee@telesat.com>
2026-04-10 19:53:23 -0400

    Enhance gcrf_to_itrf.py: add reverse conversion function from ITRF to GCRF and update command line interface

--- gcrf_to_itrf.py
+++ gcrf_to_itrf.py
@@ -63,20 +63,13 @@ def convert_gcrf_to_itrf(
     gcrf_to_itrf_rotation_matrix = earth_rotation_model.inertial_to_body_fixed_rotation(
         input_epoch_et
     )
-    # print("gcrf_to_itrf_rotation_matrix", gcrf_to_itrf_rotation_matrix)
 
     itrf_earth_rotational_velocity = (
-        earth_rotation_model.angular_velocity_in_inertial_frame(input_epoch_et)
+        earth_rotation_model.angular_velocity_in_body_fixed_frame(input_epoch_et)
     )
-    # print("itrf_earth_rotational_velocity", itrf_earth_rotational_velocity)
-
-    # print("input_gcrf_position_km", input_gcrf_position_km)
 
     input_gcrf_position = input_gcrf_position_km * 1000.0  # Convert to meters
-    # print("input_gcrf_position", input_gcrf_position)
-
     output_itrf_position = gcrf_to_itrf_rotation_matrix @ input_gcrf_position
-    # print("output_itrf_position", output_itrf_position)
 
     output_itrf_position_km = (
         output_itrf_position / 1000.0
@@ -86,25 +79,69 @@ def convert_gcrf_to_itrf(
 
     if input_gcrf_velocity_kms is not None:
         input_gcrf_velocity = input_gcrf_velocity_kms * 1000.0  # Convert to m/s
-        # print("input_gcrf_velocity", input_gcrf_velocity)
 
         output_itrf_velocity = (
             gcrf_to_itrf_rotation_matrix @ input_gcrf_velocity
             - np.cross(itrf_earth_rotational_velocity, output_itrf_position)
         )
-        # print("output_itrf_velocity", output_itrf_velocity)
         output_itrf_velocity_kms = output_itrf_velocity / 1000.0  # Convert back to km/s
 
     return output_itrf_position_km, output_itrf_velocity_kms
 
 
+# Function to convert position and velocity from ITRF to GCRF at a given epoch using the Earth rotation model
+def convert_itrf_to_gcrf(
+    earth_rotation_model,
+    input_epoch_et,
+    input_itrf_position_km,
+    input_itrf_velocity_kms=None,
+):
+    # Get rotation matrix for ITRS to GCRS transformation at the given epoch
+    # GCRF is an inertial frame, ITRF is a body-fixed frame, so this rotation matrix accounts for Earth's rotation at the given epoch
+    # Thus the name of the function is `body_fixed_to_inertial_rotation'
+    itrf_to_gcrf_rotation_matrix = earth_rotation_model.body_fixed_to_inertial_rotation(
+        input_epoch_et
+    )
+
+    gcrf_earth_rotational_velocity = (
+        earth_rotation_model.angular_velocity_in_inertial_frame(input_epoch_et)
+    )
+
+    input_itrf_position = input_itrf_position_km * 1000.0  # Convert to meters
+    output_gcrf_position = itrf_to_gcrf_rotation_matrix @ input_itrf_position
+
+    output_gcrf_position_km = (
+        output_gcrf_position / 1000.0
+    )  # Convert back to kilometers
+
+    output_gcrf_velocity_kms = None
+
+    if input_itrf_velocity_kms is not None:
+        input_itrf_velocity = input_itrf_velocity_kms * 1000.0  # Convert to m/s
+
+        output_gcrf_velocity = (
+            itrf_to_gcrf_rotation_matrix @ input_itrf_velocity
+            + np.cross(gcrf_earth_rotational_velocity, output_gcrf_position)
+        )
+        output_gcrf_velocity_kms = output_gcrf_velocity / 1000.0  # Convert back to km/s
+
+    return output_gcrf_position_km, output_gcrf_velocity_kms
+
+
 def main():
+
+    # Check options command line arguments
+    # See if -r option was given to specify reverse conversion (ITRF to GCRF instead of GCRF to ITRF) and set a flag accordingly
+    set_reverse_conversion = False
+    if len(sys.argv) > 1 and sys.argv[1] == "-r":
+        set_reverse_conversion = True
+        sys.argv.pop(1)  # Remove the -r option from the arguments list
 
     # Read time, 3d position and optionally 3d velocity from command line arguments
 
     if len(sys.argv) != 5 and len(sys.argv) != 8:
         print(
-            "Usage: python gcrf_to_itrf.py <time> <x_km> <y_km> <z_km> [ <vx_km/s> <vy_km/s> <vz_km/s> ]"
+            "Usage: python gcrf_to_itrf.py [-r] <time> <x_km> <y_km> <z_km> [ <vx_km/s> <vy_km/s> <vz_km/s> ]"
         )
         sys.exit(1)
 
@@ -118,10 +155,10 @@ def main():
 
     # Read position and velocity from command line arguments
 
-    input_gcrf_position_km = np.array([float(x) for x in sys.argv[2:5]])
-    input_gcrf_velocity_kms = None
+    input_position_km = np.array([float(x) for x in sys.argv[2:5]])
+    input_velocity_kms = None
     if len(sys.argv) == 8:
-        input_gcrf_velocity_kms = np.array([float(x) for x in sys.argv[5:8]])
+        input_velocity_kms = np.array([float(x) for x in sys.argv[5:8]])
 
     # Create Earth rotation model
 
@@ -129,27 +166,35 @@ def main():
 
     # Call the conversion function
 
-    output_itrf_position_km, output_itrf_velocity_kms = convert_gcrf_to_itrf(
-        earth_rotation_model,
-        input_epoch_et,
-        input_gcrf_position_km,
-        input_gcrf_velocity_kms,
-    )
+    if set_reverse_conversion:
+        output_position_km, output_velocity_kms = convert_itrf_to_gcrf(
+            earth_rotation_model,
+            input_epoch_et,
+            input_position_km,
+            input_velocity_kms,
+        )
+    else:
+        output_position_km, output_velocity_kms = convert_gcrf_to_itrf(
+            earth_rotation_model,
+            input_epoch_et,
+            input_position_km,
+            input_velocity_kms,
+        )
 
     print(
         input_time_string,
-        output_itrf_position_km[0],
-        output_itrf_position_km[1],
-        output_itrf_position_km[2],
+        output_position_km[0],
+        output_position_km[1],
+        output_position_km[2],
         end="",
     )
 
-    if output_itrf_velocity_kms is not None:
+    if output_velocity_kms is not None:
         print(
             " ",
-            output_itrf_velocity_kms[0],
-            output_itrf_velocity_kms[1],
-            output_itrf_velocity_kms[2],
+            output_velocity_kms[0],
+            output_velocity_kms[1],
+            output_velocity_kms[2],
             end="",
         )
     print()
