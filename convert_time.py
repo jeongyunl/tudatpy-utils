c4e814e1da0d98b8dd1557b888e78ab1735c70f1
Jeongyun Lee <jlee@telesat.com>
2026-04-11 18:03:25 -0400

    Enhance time conversion functionality: add TaiTimeData class and update conversion methods for TAI format

--- convert_time.py
+++ convert_time.py
@@ -5,14 +5,16 @@ import sys
 from dataclasses import dataclass
 from typing import Iterable
 
-from tudatpy.astro.time_representation import DateTime
+import tudatpy.astro.time_representation as time_representation
+from tudatpy.astro.time_representation import DateTime, TimeScales
 from enum import StrEnum
 
 
 class TimeFormat(StrEnum):
-    UTC_ISO = "iso"  # ISO 8601 format: "YYYY-MM-DDTHH:MM:SS.sss"
-    UTC_YMDHMS = "ymdhms"  # Year, Month, Day, Hour, Minute, Seconds format: "YYYY,MM,DD,HH,MM,SS.sss"
-    UTC_J2000 = "j2000"  # Time in UTC; in seconds since J2000 epoch (January 1, 2000, 12:00:00 UTC)
+    UTC_ISO = "iso"  # ISO 8601 format in UTC: "YYYY-MM-DDTHH:MM:SS.sss"
+    UTC_YMDHMS = "ymdhms"  # Year, Month, Day, Hour, Minute, Seconds format in UTC: "YYYY,MM,DD,HH,MM,SS.sss"
+    UTC_J2000 = "j2000"  # Time in UTC; in seconds since UTC J2000 epoch (2000-01-01 12:00:00.000 UTC)
+    TAI_J2000 = "tai"  # Time in TAI; in seconds since TAI J2000 epoch (2000-01-01 12:00:00.000 TAI = 2000-01-01 11:59:28 UTC)
 
 
 SUPPORTED_FORMATS = [c.value for c in TimeFormat]
@@ -32,14 +34,19 @@ class TimeData:
     def can_convert_to(self, other_format: TimeFormat) -> bool:
         return self.time_format != other_format
 
-    def to_iso(self) -> str:
-        raise NotImplementedError("to_iso() not implemented for this time format")
+    def to_utc_iso(self) -> str:
+        raise NotImplementedError("to_utc_iso() not implemented for this time format")
 
-    def to_ymdhms(self) -> str:
-        raise NotImplementedError("to_ymdhms() not implemented for this time format")
+    def to_utc_ymdhms(self) -> str:
+        raise NotImplementedError(
+            "to_utc_ymdhms() not implemented for this time format"
+        )
 
-    def to_j2000(self) -> float:
-        raise NotImplementedError("to_j2000() not implemented for this time format")
+    def to_utc_j2000(self) -> float:
+        raise NotImplementedError("to_utc_j2000() not implemented for this time format")
+
+    def to_tai_j2000(self) -> float:
+        raise NotImplementedError("to_tai_j2000() not implemented for this time format")
 
 
 @dataclass
@@ -48,16 +55,24 @@ class UtcIsoTimeData(TimeData):
     def __init__(self, string: str):
         super().__init__(TimeFormat.UTC_ISO, string)
 
-    def to_iso(self) -> str:
+    def to_utc_iso(self) -> str:
         return self.time_string
 
-    def to_ymdhms(self) -> str:
+    def to_utc_ymdhms(self) -> str:
         self.date_time = DateTime.from_iso_string(self.time_string)
         return f"{self.date_time.year},{self.date_time.month},{self.date_time.day},{self.date_time.hour},{self.date_time.minute},{self.date_time.seconds}"
 
-    def to_j2000(self) -> float:
+    def to_utc_j2000(self) -> float:
         self.date_time = DateTime.from_iso_string(self.time_string)
         return self.date_time.to_epoch()
+
+    def to_tai_j2000(self) -> float:
+        epoch_utc = self.to_utc_j2000()
+        return time_representation.default_time_scale_converter().convert_time(
+            input_value=epoch_utc,
+            input_scale=TimeScales.utc_scale,
+            output_scale=TimeScales.tai_scale,
+        )
 
 
 @dataclass
@@ -66,22 +81,30 @@ class UtcYmdhmsTimeData(TimeData):
     def __init__(self, string: str):
         super().__init__(TimeFormat.UTC_YMDHMS, string)
 
-    def to_iso(self) -> str:
+    def to_utc_iso(self) -> str:
         ymdhms = str.split(self.time_string, sep=",", maxsplit=6)
         date_time = DateTime(
             ymdhms[0], ymdhms[1], ymdhms[2], ymdhms[3], ymdhms[4], ymdhms[5]
         )
         return date_time.to_iso_string(number_of_digits_seconds=3)
 
-    def to_ymdhms(self) -> str:
+    def to_utc_ymdhms(self) -> str:
         return self.time_string
 
-    def to_j2000(self) -> float:
+    def to_utc_j2000(self) -> float:
         ymdhms = str.split(self.time_string, sep=",", maxsplit=6)
         date_time = DateTime(
             ymdhms[0], ymdhms[1], ymdhms[2], ymdhms[3], ymdhms[4], ymdhms[5]
         )
         return date_time.to_epoch()
+
+    def to_tai_j2000(self) -> float:
+        epoch_utc = self.to_utc_j2000()
+        return time_representation.default_time_scale_converter().convert_time(
+            input_value=epoch_utc,
+            input_scale=TimeScales.utc_scale,
+            output_scale=TimeScales.tai_scale,
+        )
 
 
 @dataclass
@@ -90,15 +113,51 @@ class UtcJ2000TimeData(TimeData):
     def __init__(self, string: str):
         super().__init__(TimeFormat.UTC_J2000, string)
 
-    def to_iso(self) -> str:
+    def to_utc_iso(self) -> str:
         date_time = DateTime.from_epoch(float(self.time_string))
         return date_time.to_iso_string(number_of_digits_seconds=3)
 
-    def to_ymdhms(self) -> str:
+    def to_utc_ymdhms(self) -> str:
         date_time = DateTime.from_epoch(float(self.time_string))
         return f"{date_time.year},{date_time.month},{date_time.day},{date_time.hour},{date_time.minute},{date_time.seconds}"
 
-    def to_j2000(self) -> float:
+    def to_utc_j2000(self) -> float:
+        return float(self.time_string)
+
+    def to_tai_j2000(self) -> float:
+        epoch_utc = float(self.time_string)
+        return time_representation.default_time_scale_converter().convert_time(
+            input_value=epoch_utc,
+            input_scale=TimeScales.utc_scale,
+            output_scale=TimeScales.tai_scale,
+        )
+
+
+@dataclass
+class TaiTimeData(TimeData):
+
+    def __init__(self, string: str):
+        super().__init__(TimeFormat.TAI_J2000, string)
+
+    def to_utc_iso(self) -> str:
+        epoch_utc = self.to_utc_j2000()
+        date_time = DateTime.from_epoch(epoch_utc)
+        return date_time.to_iso_string(number_of_digits_seconds=3)
+
+    def to_utc_ymdhms(self) -> str:
+        epoch_utc = self.to_utc_j2000()
+        date_time = DateTime.from_epoch(epoch_utc)
+        return f"{date_time.year},{date_time.month},{date_time.day},{date_time.hour},{date_time.minute},{date_time.seconds}"
+
+    def to_utc_j2000(self) -> float:
+        epoch_tai = float(self.time_string)
+        return time_representation.default_time_scale_converter().convert_time(
+            input_value=epoch_tai,
+            input_scale=TimeScales.tai_scale,
+            output_scale=TimeScales.utc_scale,
+        )
+
+    def to_tai_j2000(self) -> float:
         return float(self.time_string)
 
 
@@ -115,7 +174,10 @@ def parse_args() -> argparse.Namespace:
         + ": UTC. Year, Month, Day, Hour, Minute, Seconds format (e.g., '2024,06,01,12,00,00.000')\n"
         + "  "
         + TimeFormat.UTC_J2000
-        + ": UTC. Seconds since J2000 epoch (January 1, 2000, 12:00:00 UTC) (e.g., '31557600.000')\n",
+        + ": UTC. Seconds since J2000 epoch (January 1, 2000, 12:00:00 UTC) (e.g., '31557600.000')\n"
+        + "  "
+        + TimeFormat.TAI_J2000
+        + ": TAI",
     )
     parser.add_argument(
         "-i",
@@ -158,17 +220,21 @@ def parse_time_value(value: str, fmt: str) -> TimeData:
         return UtcYmdhmsTimeData(value)
     if fmt == TimeFormat.UTC_J2000:
         return UtcJ2000TimeData(value)
+    if fmt == TimeFormat.TAI_J2000:
+        return TaiTimeData(value)
     raise ValueError(f"Unsupported input format: {fmt}")
 
 
-def convert_time_value(dt: TimeData, fmt: str) -> str:
-    if fmt == TimeFormat.UTC_ISO:
-        return dt.to_iso()
-    if fmt == TimeFormat.UTC_YMDHMS:
-        return dt.to_ymdhms()
-    if fmt == TimeFormat.UTC_J2000:
-        return str(dt.to_j2000())
-    raise ValueError(f"Unsupported output format: {fmt}")
+def convert_time_value(time: TimeData, format_name: str) -> str:
+    if format_name == TimeFormat.UTC_ISO:
+        return time.to_utc_iso()
+    if format_name == TimeFormat.UTC_YMDHMS:
+        return time.to_utc_ymdhms()
+    if format_name == TimeFormat.UTC_J2000:
+        return str(time.to_utc_j2000())
+    if format_name == TimeFormat.TAI_J2000:
+        return str(time.to_tai_j2000())
+    raise ValueError(f"Unsupported output format: {format_name}")
 
 
 def main() -> None:
