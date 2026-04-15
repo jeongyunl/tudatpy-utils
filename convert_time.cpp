1b298fb8bc23d40ea405778a9fe2a41f2f7b420d
Jeongyun Lee <jeongyun@gmail.com>
2026-04-15 17:44:39 -0400

    Refactor Handler class: streamline constructor and improve dispatch function handling

--- convert_time.cpp
+++ convert_time.cpp
@@ -52,32 +52,45 @@ TimeFormat parse_time_format(const std::string& format_str)
 typedef std::variant<double, std::string> TimeValue;
 
 using DispatchKey = std::pair<TimeFormat, TimeFormat>;
-using Handler = std::function<TimeValue(const TimeValue&)>;
-
-// Wrap a strongly-typed conversion function into a generic dispatch handler.
-// Supports both signatures:
-//   Ret f(Arg)            (e.g. double f(double))
-//   Ret f(const Arg&)     (e.g. double f(const std::string&))
-
-template <typename Arg, typename Ret>
-Handler make_handler(Ret (*func)(Arg))
+class Handler
 {
-	return [func](const TimeValue& input_time) -> TimeValue {
-		Arg arg = std::get<Arg>(input_time);
-		Ret out = func(arg);
-		return TimeValue{ std::in_place_type<Ret>, std::move(out) };
-	};
-}
+public:
+	using WrappedCallable = std::function<TimeValue(const TimeValue&)>;
 
-template <typename Arg, typename Ret>
-Handler make_handler(Ret (*func)(const Arg&))
-{
-	return [func](const TimeValue& input_time) -> TimeValue {
-		const auto& arg = std::get<Arg>(input_time);
-		Ret out = func(arg);
-		return TimeValue{ std::in_place_type<Ret>, std::move(out) };
-	};
-}
+	Handler() = default;
+
+	explicit Handler(WrappedCallable callable)
+		: callable_(std::move(callable))
+	{
+	}
+
+	template <typename Arg, typename Ret>
+	Handler(Ret (*func)(Arg))
+		: callable_([func](const TimeValue& input_time) -> TimeValue {
+			Arg arg = std::get<Arg>(input_time);
+			Ret out = func(arg);
+			return TimeValue{ std::in_place_type<Ret>, std::move(out) };
+		})
+	{
+	}
+
+	template <typename Arg, typename Ret>
+	Handler(Ret (*func)(const Arg&))
+		: callable_([func](const TimeValue& input_time) -> TimeValue {
+			const auto& arg = std::get<Arg>(input_time);
+			Ret out = func(arg);
+			return TimeValue{ std::in_place_type<Ret>, std::move(out) };
+		})
+	{
+	}
+
+	TimeValue operator()(const TimeValue& input_time) const { return callable_(input_time); }
+
+	explicit operator bool() const { return static_cast<bool>(callable_); }
+
+private:
+	WrappedCallable callable_;
+};
 
 std::shared_ptr<tudat::earth_orientation::TerrestrialTimeScaleConverter> tudat_time_scale_converter = nullptr;
 
@@ -654,65 +667,61 @@ double tdb_apx_tudat_to_tdb_apx_tudat(const double tdb_apx_tudat_epoch)
 }
 
 std::map<DispatchKey, Handler> dispatchTable{
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_ISO_TUDAT },
-	  make_handler(utc_iso_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_POSIX }, make_handler(utc_iso_tudat_to_utc_posix) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(utc_iso_tudat_to_utc_tudat) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(utc_iso_tudat_to_tai_tudat) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TT_TUDAT }, make_handler(utc_iso_tudat_to_tt_tudat) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(utc_iso_tudat_to_tdb_tudat) },
-	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TDB_APX_TUDAT },
-	  make_handler(utc_iso_tudat_to_tdb_apx_tudat) },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_ISO_TUDAT }, utc_iso_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_POSIX }, utc_iso_tudat_to_utc_posix },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::UTC_TUDAT }, utc_iso_tudat_to_utc_tudat },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TAI_TUDAT }, utc_iso_tudat_to_tai_tudat },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TT_TUDAT }, utc_iso_tudat_to_tt_tudat },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TDB_TUDAT }, utc_iso_tudat_to_tdb_tudat },
+	{ { TimeFormat::UTC_ISO_TUDAT, TimeFormat::TDB_APX_TUDAT }, utc_iso_tudat_to_tdb_apx_tudat },
 
-	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_ISO_TUDAT }, make_handler(utc_posix_to_utc_iso_tudat) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_POSIX }, make_handler(utc_posix_to_utc_posix) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_TUDAT }, make_handler(utc_posix_to_utc_tudat) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::TAI_TUDAT }, make_handler(utc_posix_to_tai_tudat) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::TT_TUDAT }, make_handler(utc_posix_to_tt_tudat) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::TDB_TUDAT }, make_handler(utc_posix_to_tdb_tudat) },
-	{ { TimeFormat::UTC_POSIX, TimeFormat::TDB_APX_TUDAT }, make_handler(utc_posix_to_tdb_apx_tudat) },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_ISO_TUDAT }, utc_posix_to_utc_iso_tudat },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_POSIX }, utc_posix_to_utc_posix },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::UTC_TUDAT }, utc_posix_to_utc_tudat },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::TAI_TUDAT }, utc_posix_to_tai_tudat },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::TT_TUDAT }, utc_posix_to_tt_tudat },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::TDB_TUDAT }, utc_posix_to_tdb_tudat },
+	{ { TimeFormat::UTC_POSIX, TimeFormat::TDB_APX_TUDAT }, utc_posix_to_tdb_apx_tudat },
 
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_ISO_TUDAT }, make_handler(utc_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_POSIX }, make_handler(utc_tudat_to_utc_posix) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(utc_tudat_to_utc_tudat) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(utc_tudat_to_tai_tudat) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::TT_TUDAT }, make_handler(utc_tudat_to_tt_tudat) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(utc_tudat_to_tdb_tudat) },
-	{ { TimeFormat::UTC_TUDAT, TimeFormat::TDB_APX_TUDAT }, make_handler(utc_tudat_to_tdb_apx_tudat) },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_ISO_TUDAT }, utc_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_POSIX }, utc_tudat_to_utc_posix },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::UTC_TUDAT }, utc_tudat_to_utc_tudat },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::TAI_TUDAT }, utc_tudat_to_tai_tudat },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::TT_TUDAT }, utc_tudat_to_tt_tudat },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::TDB_TUDAT }, utc_tudat_to_tdb_tudat },
+	{ { TimeFormat::UTC_TUDAT, TimeFormat::TDB_APX_TUDAT }, utc_tudat_to_tdb_apx_tudat },
 
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_ISO_TUDAT }, make_handler(tai_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_POSIX }, make_handler(tai_tudat_to_utc_posix) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(tai_tudat_to_utc_tudat) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(tai_tudat_to_tai_tudat) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::TT_TUDAT }, make_handler(tai_tudat_to_tt_tudat) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(tai_tudat_to_tdb_tudat) },
-	{ { TimeFormat::TAI_TUDAT, TimeFormat::TDB_APX_TUDAT }, make_handler(tai_tudat_to_tdb_apx_tudat) },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_ISO_TUDAT }, tai_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_POSIX }, tai_tudat_to_utc_posix },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::UTC_TUDAT }, tai_tudat_to_utc_tudat },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::TAI_TUDAT }, tai_tudat_to_tai_tudat },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::TT_TUDAT }, tai_tudat_to_tt_tudat },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::TDB_TUDAT }, tai_tudat_to_tdb_tudat },
+	{ { TimeFormat::TAI_TUDAT, TimeFormat::TDB_APX_TUDAT }, tai_tudat_to_tdb_apx_tudat },
 
-	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_ISO_TUDAT }, make_handler(tt_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_POSIX }, make_handler(tt_tudat_to_utc_posix) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(tt_tudat_to_utc_tudat) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(tt_tudat_to_tai_tudat) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::TT_TUDAT }, make_handler(tt_tudat_to_tt_tudat) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(tt_tudat_to_tdb_tudat) },
-	{ { TimeFormat::TT_TUDAT, TimeFormat::TDB_APX_TUDAT }, make_handler(tt_tudat_to_tdb_apx_tudat) },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_ISO_TUDAT }, tt_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_POSIX }, tt_tudat_to_utc_posix },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::UTC_TUDAT }, tt_tudat_to_utc_tudat },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::TAI_TUDAT }, tt_tudat_to_tai_tudat },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::TT_TUDAT }, tt_tudat_to_tt_tudat },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::TDB_TUDAT }, tt_tudat_to_tdb_tudat },
+	{ { TimeFormat::TT_TUDAT, TimeFormat::TDB_APX_TUDAT }, tt_tudat_to_tdb_apx_tudat },
 
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_ISO_TUDAT }, make_handler(tdb_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_POSIX }, make_handler(tdb_tudat_to_utc_posix) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(tdb_tudat_to_utc_tudat) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(tdb_tudat_to_tai_tudat) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::TT_TUDAT }, make_handler(tdb_tudat_to_tt_tudat) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(tdb_tudat_to_tdb_tudat) },
-	{ { TimeFormat::TDB_TUDAT, TimeFormat::TDB_APX_TUDAT }, make_handler(tdb_tudat_to_tt_tudat) },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_ISO_TUDAT }, tdb_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_POSIX }, tdb_tudat_to_utc_posix },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::UTC_TUDAT }, tdb_tudat_to_utc_tudat },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::TAI_TUDAT }, tdb_tudat_to_tai_tudat },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::TT_TUDAT }, tdb_tudat_to_tt_tudat },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::TDB_TUDAT }, tdb_tudat_to_tdb_tudat },
+	{ { TimeFormat::TDB_TUDAT, TimeFormat::TDB_APX_TUDAT }, tdb_tudat_to_tt_tudat },
 
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_ISO_TUDAT },
-	  make_handler(tdb_apx_tudat_to_utc_iso_tudat) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_POSIX }, make_handler(tdb_apx_tudat_to_utc_posix) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_TUDAT }, make_handler(tdb_apx_tudat_to_utc_tudat) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TAI_TUDAT }, make_handler(tdb_apx_tudat_to_tai_tudat) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TT_TUDAT }, make_handler(tdb_apx_tudat_to_tt_tudat) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TDB_TUDAT }, make_handler(tdb_apx_tudat_to_tdb_tudat) },
-	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TDB_APX_TUDAT },
-	  make_handler(tdb_apx_tudat_to_tdb_apx_tudat) },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_ISO_TUDAT }, tdb_apx_tudat_to_utc_iso_tudat },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_POSIX }, tdb_apx_tudat_to_utc_posix },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::UTC_TUDAT }, tdb_apx_tudat_to_utc_tudat },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TAI_TUDAT }, tdb_apx_tudat_to_tai_tudat },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TT_TUDAT }, tdb_apx_tudat_to_tt_tudat },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TDB_TUDAT }, tdb_apx_tudat_to_tdb_tudat },
+	{ { TimeFormat::TDB_APX_TUDAT, TimeFormat::TDB_APX_TUDAT }, tdb_apx_tudat_to_tdb_apx_tudat },
 };
 
 int main(int argc, char* argv[])
